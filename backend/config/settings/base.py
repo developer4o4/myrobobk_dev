90e7cbcb32120e12f6aba7b7a8b60f03ebf0c35a
izzatillo <adxamovizzatillo008@gmail.com>
2026-03-01 14:15:44 +0500

    port rewrite

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY","dev")
 DEBUG = os.getenv("DEBUG","1")=="1"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS","*").split(",")
-
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
