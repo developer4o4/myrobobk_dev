47968f254e4a0f97838087c46726335032e50ac4
izzatillo <adxamovizzatillo008@gmail.com>
2026-03-17 23:37:22 +0500

    a

--- backend/apps/teachers/serializers.py
+++ backend/apps/teachers/serializers.py
@@ -14,6 +14,7 @@ class TeacherListSerializer(serializers.ModelSerializer):
             "job",
             "direction",
             "experience",
+            "about",
             "work_place",
             "img",
             "slug",
